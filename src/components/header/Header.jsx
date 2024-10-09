bbb62432c16dc9e5f38856965815ae3c19e519eb
Ramses <illove500700@gmail.com>
2024-10-10 00:00:17 +0600

    - bugs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -54,6 +54,8 @@ const Header = () => {
       )} */}
 
       {/* Login Button */}
+      <p className="hidden lg:block text-white bg-[#333335] rounded-[45px] text-2xl px-[27px] py-[4px] hover: transition-all duration-300 ease-in-out">
+      </p>
     </header>
   );
 };
