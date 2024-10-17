7377a097a7ca8cf14f701e102e820d86f8138765
kasym-r <kasymrakhmatyllo@gmail.com>
2024-10-17 22:21:11 +0600

    commit

--- src/pages/home/resources/Resources.jsx
+++ src/pages/home/resources/Resources.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import img from "../../../assets/Rectangle 4541.svg";
+import img from "../../../assets/Rectangle.svg";
+import img1 from "../../../assets/Rectangle1.svg";
+import img2 from "../../../assets/Rectangle2.svg";
+import img3 from "../../../assets/Rectangle3.svg";
 
 const Resources = () => {
   return (
     <>
-      <div className="flex">
+      <div className="flex gap-[40px]">
         <div>
           <p>Полные ресурсы для победы в олимпиадах</p>
-          <img src={img} alt="" />
+          <img src={img} />
         </div>
         <div>
           <p>
@@ -21,7 +24,11 @@ const Resources = () => {
             истории. Мы стремимся сделать обучение простым и увлекательным,
             предоставляя все необходимые инструменты для достижения успеха.
           </p>
-
+          <div className="grid grid-cols-2">
+            <img src={img1} className="col-span-2" />
+            <img src={img2} />
+            <img src={img3} />
+          </div>
         </div>
       </div>
     </>
