efd3811bf76a4c9a196bf9fce541057a79794c2c
kasym-r <kasymrakhmatyllo@gmail.com>
2024-11-27 22:40:55 +0600

    bugs check

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -17,7 +17,7 @@ const Card = ({ img, text, price }) => {
 
       <div
         ref={ref}
-        className={`bg-white w-[424px] rounded-[15px]  flex flex-col p-[20px] gap-4 
+        className={`bg-white  rounded-[15px]  flex flex-col p-[20px] gap-4 
       transition-opacity duration-700 ease-in-out 
       transform ${inView ? "opacity-100" : "opacity-0"}`}
       >
--- src/pages/home/articles/Articles.jsx
+++ src/pages/home/articles/Articles.jsx
@@ -36,13 +36,13 @@ const Articles = () => {
       <h2 className="text-white font-unbounded font-medium text-5xl mb-10">
         СТАТЬИ
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-5 lg:grid-cols-3 lg:gap-11">
         {cards.map((card) => (
           <Card
             key={card.id}
             img={card.image}
             text={card.title}
-            price={"1500"} 
+            price={"1500"}
           />
         ))}
       </div>
