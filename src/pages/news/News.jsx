433a629a889794c61a18c05eef184475ac85c4a9
kasym-r <kasymrakhmatyllo@gmail.com>
2024-12-14 14:29:47 +0600

    commit

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,14 +1,12 @@
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import NewsText from "./newsText/NewsText";
-import Info from "./info/Info";
 
 const News = () => {
 
   return (
     <>
       <Header />
-      {/* <Info /> */}
       <NewsText/>
       <Footer />
     </>
