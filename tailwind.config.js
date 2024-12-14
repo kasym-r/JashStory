7ae71de12aa59db183a9568a556b85f2356afefe
kasym-r <kasymrakhmatyllo@gmail.com>
2024-12-14 14:46:19 +0600

    deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-module.exports = {
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // This should cover your component files
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
