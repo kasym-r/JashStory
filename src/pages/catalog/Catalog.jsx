cab049e4ca574ed6d6c0f37fe6cd580144cb4062
kasym-r <kasymrakhmatyllo@gmail.com>
2024-12-16 02:38:58 +0600

    search button function

--- src/pages/catalog/Catalog.jsx
+++ src/pages/catalog/Catalog.jsx
@@ -7,6 +7,10 @@ import search from '../../assets/searchIcon.svg'
 
 const Catalog = () => {
   const [cards, setCards] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isAccordionOpen, setAccordionOpen] = useState(false);
+
+  const toggleAccordion = () => setAccordionOpen(!isAccordionOpen);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,6 +26,10 @@ const Catalog = () => {
     fetchData();
   }, []);
   
+  const filteredCards = cards.filter((card) =>
+    card.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <>
       <Header />
@@ -32,17 +40,37 @@ const Catalog = () => {
           </p>
         </div>
       </div>
-      <div className="relative my-auto flex justify-center items-center mx-[40px] mt-14">
+      <div className="relative my-auto flex gap-3 items-center mx-[40px] mt-14 font-sf font-light">
         <input
           type="text"
-          className="w-screen py-2 pr-14 pl-4 border h-[60px] border-black rounded-[20px] focus:outline-none"
+          className="w-screen relative py-2 pr-14 pl-4 border h-[60px] border-black rounded-[20px] focus:outline-none"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Поиск по названию"
         />
         <button className="absolute right-[7px] top-1/2 transform -translate-y-1/2 text-gray-500">
           <img src={search} />
         </button>
+        {/* <div className="">
+          <button
+            onClick={toggleAccordion}
+            className=" py-2 px-4 bg-white rounded-[40px] border border-black"
+          >
+            Где?
+          </button>
+          {isAccordionOpen && (
+            <div className="absolute z-10 right-0 top-[100%] bg-white shadow-md transtion-all duration-300 ease-in-out">
+              <ul>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer transtion-all duration-300 ease-in-out">Category 1</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer transtion-all duration-300 ease-in-out">Category 2</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer transtion-all duration-300 ease-in-out">Category 3</li>
+              </ul>
+            </div>
+          )}
+        </div> */}
       </div>
       <div className="m-10 grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-5 lg:grid-cols-3 lg:gap-11">
-      {cards.map((card) => (
+      {filteredCards.map((card) => (
           <Card
             id={card.id}
             key={card.id}
